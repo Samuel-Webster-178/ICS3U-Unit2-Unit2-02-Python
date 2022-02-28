ef88cafbd38af06c8e3581c9a1b883bddcecbb8d
Samuel-Webster-178 <samuel.webster@mths.ca>
2022-02-28 19:49:07 +0000

    unfinished

---
+++ math_with_design.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by Samuel Webster
+# Created on February 2022
+# Calculates perimeter and area of a rectangle
+#     with dimensions inputted from user
+
+
+def main():
+
+    print("If a rectangle has the dimensions: 5cm x 3cm")
+    print("Area is {}cm².".format(3 * 5))
+    print("Perimeter is {}cm.".format(2 * (3 + 5)))
+    
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
